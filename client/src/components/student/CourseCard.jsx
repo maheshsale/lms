a345aafe4500c8a7dadbd2607f546e717012afa5
maheshsale <maheshsale333@gmail.com>
2025-02-12 15:16:34 +0530

    Course details left column completed

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -13,7 +13,7 @@ const CourseCard = ({course}) => {
       <img className='w-full' src={course.courseThumbnail} alt="course thumbnail" />
       <div className='p-3 text-left'>
         <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
-        <p className='text-gray-500'>{course.educator.name}</p>
+        <p className='text-gray-500'>Mahesh Sale</p>
         <div className='flex items-center space-x-2'>
           <p>{calculateRating(course)}</p>
           <div className='flex'>
